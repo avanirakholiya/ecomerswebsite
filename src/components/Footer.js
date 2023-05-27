f9a6aa675cc8c9efe36dbad471470d6c200a03d2
avanisenjaliya <avanisenjaliya1@gmail.com>
2023-05-27 11:34:50 +0530

    project complete

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Footer() {
+        
+        return (
+        <>
+            <div className='footer'>
+                <div className='contain' style={{backgroundColor:"black",color:"white",padding:"60px"}}>
+                    <center>
+                    Shopify © 2022 | All Rights Reserved.
+                    </center>
+          
+                </div>
+            </div>
+        </>
+        )
+    }
+export default Footer;
